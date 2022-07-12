b1724c1f5cf858a0463298c768bc44a7daedcda9
Nguyen Hoang Dieu <dieu.nh198217@sis.hust.edu.vn>
2022-07-12 16:19:51 +0700

    Added my project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*------------------------------------Slider-------------------------------------*/
 const header = document.querySelector("header")
     window.addEventListener("scroll",function(){
         x=window.pageYOffset
@@ -33,9 +34,40 @@ function Slider(index){
 }
 setInterval(imgSlide,5000)
 
+/*----------------------------------------Cartegory---------------------------------------*/
 const itemsliderbar = document.querySelectorAll(".cartegory-left-li")
 itemsliderbar.forEach(function(menu,index){
     menu.addEventListener("click",function(){
         menu.classList.toggle("block")
     })
 })
+
+/*---------------------------------------Product-----------------------------------------*/
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smallImg = document.querySelector(".product-content-left-small-img img")
+smallImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click", function(){
+       bigImg.src = imgItem.src 
+    })
+})
+const thuonghieu = document.querySelector(".thuonghieu")
+const chitiet = document.querySelector(".chitiet")
+if(thuonghieu){
+    thuonghieu.addEventListener("click",function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-thuonghieu").style.display = "block"
+    })
+}
+if(chitiet){
+    chitiet.addEventListener("click",function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-thuonghieu").style.display = "none"
+    })
+}
+const butTon = document.querySelector(".product-content-right-bottom-top")
+if(butTon){
+    butTon.addEventListener("click", function(){
+        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB")
+
+    })
+}
